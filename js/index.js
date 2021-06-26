d667e264fd04efbb0462acd7f93e3e10ab4d4183
Isaac Yael <sigan1998@gmail.com>
2021-06-26 13:54:58 -0500

    Primer avance

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+const tacosSuadero = document.getElementById('tacosSuadero');
+const tacosCampechanos = document.getElementById('tacosCampechanos');
+const volcanes = document.getElementById('volcanes');
+const tlacoyos = document.getElementById('tlacoyos');
+
+const addTacosSuadero = () => {
+    
+}
+
+tacosSuadero.addEventListener('click', addTacosSuadero)
