1928a9cff6d6212855db677ecc59ebce757b5a5e
Isaac Yael <sigan1998@gmail.com>
2021-06-29 00:06:38 -0500

    cambios basicos

--- js/index.js
+++ js/index.js
@@ -3,9 +3,13 @@ const tacosSuadero = document.getElementById('tacosSuadero');
 const tacosCampechanos = document.getElementById('tacosCampechanos');
 const volcanes = document.getElementById('volcanes');
 const tlacoyos = document.getElementById('tlacoyos');
+const listItem = document.getElementById('order_list')
 
 const addTacosSuadero = () => {
-    
+    firstOrder = document.createElement('li')
+    firstOrder.classList.add('list_style')
+    firstOrder.textContent = '2 tacos de Suadero'
+    listItem.append(firstOrder)
 }
 
 tacosSuadero.addEventListener('click', addTacosSuadero)
